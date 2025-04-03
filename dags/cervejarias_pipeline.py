65f152de2cbd12b5ad7c0dd64e22f250a2bbb4a7
joao_macedo_ <jpmdaaaa@gmail.com>
2025-04-03 00:30:19 -0300

    Airflow, docker-compose

--- dags/cervejarias_pipeline.py
+++ dags/cervejarias_pipeline.py
@@ -1,8 +1,11 @@
-from datetime import datetime
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import sys
+import os
+
+# Adiciona o caminho dos scripts ao PYTHONPATH
+sys.path.append(os.path.join(os.path.dirname(__file__), '../scripts'))
 
 default_args = {
     'owner': 'airflow',
@@ -13,38 +16,29 @@ default_args = {
 }
 
 def run_consumer():
-    import sys
-    sys.path.append('/opt/airflow/scripts')
     from consumidor_api import ConsumidorAPICervejarias
     consumidor = ConsumidorAPICervejarias()
     return consumidor.buscar_todas_cervejarias()
 
-def run_transform(data):
+def run_transform():
     from transformacao import TransformadorCervejarias
-    TransformadorCervejarias().processar(data)
+    TransformadorCervejarias().processar()
 
 with DAG(
     'cervejarias_pipeline',
     default_args=default_args,
     schedule_interval='@daily',
     catchup=False,
-    tags=['brewery']
 ) as dag:
-
-    start = DummyOperator(task_id='start')
 
     extract_task = PythonOperator(
         task_id='extract_data',
-        python_callable=run_consumer,
-        provide_context=True
+        python_callable=run_consumer
     )
 
     transform_task = PythonOperator(
         task_id='transform_data',
-        python_callable=run_transform,
-        op_args=["{{ ti.xcom_pull(task_ids='extract_data') }}"]
+        python_callable=run_transform
     )
 
-    end = DummyOperator(task_id='end')
-
-    start >> extract_task >> transform_task >> end
+    extract_task >> transform_task
